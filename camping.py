d22804399ae009c4396ae39eaffa1a65e61c5c44
Aminkey47 <abdizeinabey@gmail.com>
2026-05-26 15:50:53 +0300

    voila 3

---
+++ camping.py
@@ -0,0 +1,20 @@
+#we are going to camp and stuff we neeeeeeeed are alot
+#tomorrow is eid and hussein is sleeping wwwwwwww
+#husseein hajui commmmments how nowwwww
+
+#camping_stuff = "tent, sleeping bags, water, rasperry pi, coffe, knife, ethernet cable, flash drive, beard oil, marshmallows"
+
+#print(camping_stuff)
+
+#PYTHON LISTS YYYYYYYEEYYYYYY
+
+camping_lists = ["tent", "sleeping bags", "water", "rasperry pi", "coffe", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
+
+camp_site = {"Crystal Lake" , 404, 89.3, True}
+
+camping_lists.extend(["bidet", "toilet paper"])
+
+
+print(camping_lists)
+
+
